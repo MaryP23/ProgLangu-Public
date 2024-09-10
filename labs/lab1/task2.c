a573a0c706ffd4a388677940fd3c066902a9ddaa
MaryP23 <papstmaria24@gmail.com>
2024-09-10 11:10:38 +0500

    Update task2.c

--- labs/lab1/task2.c
+++ labs/lab1/task2.c
@@ -4,3 +4,15 @@ int main(){
     int c=a+b;
     printf("c=%d",c);
 }
+#include <stdio.h>
+int main(){
+    int a=4,b=30;
+    int c=a+b;
+    printf("c=%d",c);
+}
+#include <stdio.h>
+int main(){
+    int a=7,b=6;
+    int c=a+b;
+    printf("c=%d",c);
+}
