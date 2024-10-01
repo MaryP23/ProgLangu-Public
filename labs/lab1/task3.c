3169aff47c3d2a2cc29e9d5c82c2a9d96dd6d8cf
MaryP23 <papstmaria24@gmail.com>
2024-10-01 10:39:49 +0500

    Update task3.c

--- labs/lab1/task3.c
+++ labs/lab1/task3.c
@@ -1,1 +1,13 @@
-
+#include<stdio.h>
+int main(int number, char *argv[]) { 
+  if (number <2) { 
+    printf("There are no arguments for output.\n");
+    return 1; 
+    }
+  printf("Number of arguments %d\n", number - 1);
+  printf("Arguments for output:\n");
+  for (int i = 1; i < number; i++) { 
+    printf("%d: %s\n", i, argv[i]); 
+  }
+  return 0; 
+  }
