3d9adcabf0b1f20d0ad597234f700e73a9cd7646
MaryP23 <papstmaria24@gmail.com>
2024-10-08 09:50:07 +0500

    Update and rename task7 to task7.c

--- labs/lab1/task7.c
+++ labs/lab1/task7.c
@@ -4,7 +4,7 @@
 void displayFileContent(const char *filename) {
     FILE *file = fopen(filename, "r");
 
-    char line[256];
+    char line[256]; // массив новой строки
     printf("The contents of the filel: '%s':\n", filename);
     while (fgets(line, sizeof(line), file)) {
         printf("%s", line);
@@ -16,18 +16,18 @@ int main() {
     const char *filename = "task7/checktask7.txt"; 
     char newLine[256]; 
 
-    printf("The contents of the file before the changes:\n");
+    printf("The contents of the file before the changes:\n");// вывод до измениений
     displayFileContent(filename);
 
-    printf("Enter the line you want to add to the file: ");
+    printf("Enter the line you want to add to the file: ");// новая строка
     scanf(" %[^\n]", newLine); 
   
-    FILE *file = fopen(filename, "a"); 
+    FILE *file = fopen(filename, "a"); // a- добавление в конец
 
-    fprintf(file, "%s\n", newLine); 
+    fprintf(file, "%s\n", newLine); // записываем строку в файл
     fclose(file); 
 
-    printf("\nThe contents of the with the entered sentence:\n");
+    printf("\nThe contents of the with the entered sentence:\n");// вывод 
     displayFileContent(filename);
 
     return 0;
