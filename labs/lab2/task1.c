794729d7e0eec8a9508db60fd99ad7fd228508f7
MaryP23 <papstmaria24@gmail.com>
2024-10-28 23:38:48 +0500

    Update task1.c

--- labs/lab2/task1.c
+++ labs/lab2/task1.c
@@ -1,27 +1,28 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <time.h> 
+#include <stdio.h> //ввод-вывод
+#include <stdlib.h> //стандартные функции
+#include <time.h> //работа с временем
 //Написать программу, которая генерирует массив из псевдослучайных целых чисел и выводит его в консоль.
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
+    if (argc < 2) {// первый элемент название программы 
         printf("The size of the array is not entered\n");
         return 1; // Завершаем программу с кодом ошибки
     }
 
-    int size = atoi(argv[1]); 
+    int size = atoi(argv[1]); // 0-название программы , atoi- преобразует строку в число
     if (size <= 0) {
         printf("Invalid size of the array\n");
         return 1; // Завершаем программу с кодом ошибки
     }
 
-    float *randomFloats = malloc(size * sizeof(float)); 
+    float *randomFloats = malloc(size * sizeof(float)); //добавление размера массива
     if (randomFloats == NULL) {
         printf("Memory allocation failed\n");
         return 1; // Завершаем программу с кодом ошибки
     }
 
-    srand(time(NULL));
+    srand(time(NULL)); // инициализация генератора случайных чисел, возвращает время с начала эпохи
+    // stand устанавливает изначальное значение, это все позволяет генерировать разные последовательности
     for (int i = 0; i < size; i++) {
         randomFloats[i] = (float)rand() / RAND_MAX; 
     }
