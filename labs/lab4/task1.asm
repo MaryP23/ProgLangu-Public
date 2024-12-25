f7ff5deb177c553fe2d9a13c586b20bbd143d2c0
MaryP23 <papstmaria24@gmail.com>
2024-12-25 10:53:59 +0500

    Update task1.asm

--- labs/lab4/task1.asm
+++ labs/lab4/task1.asm
@@ -1,17 +1,11 @@
-section .data
-	hello:     db 'Hello, World!',10    ; 'Hello, World!' plus a linefeed character
-	helloLen:  equ $-hello             ; Length of the 'Hello world!' string
-/*Написать программу, выводящую строку Hello World*/
-section .text
-	global _start
+; Программа для вывода "Hello World" на экран LC-3
+.ORIG x3000        ; Начало программы
 
-_start:
-	mov eax,4            ; The system call for write (sys_write)
-	mov ebx,1            ; File descriptor 1 - standard output
-	mov ecx,hello        ; Put the offset of hello in ecx
-	mov edx,helloLen     ; helloLen is a constant, so we don't need to say
-	                     ;  mov edx,[helloLen] to get it's actual value
-	int 80h              ; Call the kernel
-	mov eax,1            ; The system call for exit (sys_exit)
-	mov ebx,0            ; Exit with return "code" of 0 (no error)
-	int 80h;
+LEA R0, STRING     ; Загружаем адрес строки в R0
+PUTS               ; Вывод строки на экран
+
+HALT               ; Остановка программы
+
+STRING .STRINGZ "Hello World" ; Определение строки
+
+.END
